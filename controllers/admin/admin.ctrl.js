9fb250deb2cf647493408e78dab578f625a98200
ParkChulMin <fe26ming@gmail.com>
2021-06-26 15:53:31 +0900

    async await

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,32 +1,28 @@
 const models = require('../../models');
 
-exports.get_products = ( _ , res) => {
-    models.Products.findAll({
+exports.get_products = async ( _ , res) => {
+    try{
+        const products =  await models.Products.findAll();
+        res.render( 'admin/products.html' ,{ products });
+    
+    }catch(err){
 
-    }).then( (products) => {
-        // DB에서 받은 products를 products변수명으로 내보냄
-        res.render( 'admin/products.html' ,{ products : products });
-    });
+    }
+  
 }
 
 exports.get_products_write = ( _ , res) => {
     res.render( 'admin/write.html');
 }
 
-exports.post_products_write = ( req , res ) => {
-    models.Products.create({
-        name : req.body.name,
-        price : req.body.price ,
-        description : req.body.description
-    }).then( () => {
-        res.redirect('/admin/products');
-    });
+exports.post_products_write = async ( req , res ) => {    
+    await models.Products.create(req.body);
+    res.redirect('/admin/products');
 }
 
-exports.get_products_detail = ( req , res ) => {
-    models.Products.findByPk(req.params.id).then( (product) => {
-        res.render('admin/detail.html', { product: product });  
-    });
+exports.get_products_detail = async ( req , res ) => {  
+    const product = await models.Products.findByPk(req.params.id);
+    res.render('admin/detail.html', { product: product });  
 };
 
 exports.get_products_edit = ( req , res ) => {
@@ -36,29 +32,25 @@ exports.get_products_edit = ( req , res ) => {
     });
 };
 
-exports.post_products_edit = ( req , res ) => {
+exports.post_products_edit = async ( req , res ) => {
 
-    models.Products.update(
-        {
-            name : req.body.name,
-            price : req.body.price ,
-            description : req.body.description
-        }, 
+    await models.Products.update(
+        req.body , 
         { 
             where : { id: req.params.id } 
         }
-    ).then( () => {
-        res.redirect('/admin/products/detail/' + req.params.id );
-    });
+    );
+
+    res.redirect('/admin/products/detail/' + req.params.id );
 
 }
 
-exports.get_products_delete = ( req , res ) => {
-    models.Products.destroy({
+exports.get_products_delete = async ( req , res ) => {
+    await models.Products.destroy({
         where : {
             id : req.params.id
         }
-    }).then( () => {
-        res.redirect('/admin/products');
     })
+    res.redirect('/admin/products');
+    
 };
