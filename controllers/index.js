9a9dd9a91be94f7655928a36d5859f5e6c5dd58e
ParkChulMin <fe26ming@gmail.com>
2021-07-20 23:59:28 +0900

    탬플릿 작성

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,7 @@ const { Router } = require('express');
 const router = Router()
 
 router.use('/admin', require('./admin'));
+router.use('/accounts' , require('./accounts'));
+
 
 module.exports = router;
